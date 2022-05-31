ed09fe483e04c5c1240b3d506a72342a92781ad8
Gabriel Souza <gabrielsouza.dev@hotmail.com>
2022-05-31 15:39:04 -0300

    31/05

---
+++ ProjetoPHP/vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => 'eb339ed384ab2f5b03d329c53e031173a0581e97',
+        'name' => 'aluno/projeto-php',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'aluno/projeto-php' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => 'eb339ed384ab2f5b03d329c53e031173a0581e97',
+            'dev_requirement' => false,
+        ),
+    ),
+);
